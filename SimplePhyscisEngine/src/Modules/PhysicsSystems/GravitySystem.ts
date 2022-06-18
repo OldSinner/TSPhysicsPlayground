5a057246b95416fb4affab538bd3ef89d919cbbf
OldSinner <antylifepatia@gmail.com>
2022-06-18 13:18:08 +0200

    repair gravity sytstem n-body problem

--- SimplePhyscisEngine/src/Modules/PhysicsSystems/GravitySystem.ts
+++ SimplePhyscisEngine/src/Modules/PhysicsSystems/GravitySystem.ts
@@ -6,8 +6,8 @@ import { RigidBodyContext } from "../Context/RigidBodyContext";
 import TypeCheck from "../Utils/TypeCheck";
 export default class GravitySystem {
   private _object: PhysicObject;
-  _gravityType: GravityTypes = GravityTypes.EarthLike;
-  _gravityAttraction: number = 50;
+  _gravityType: GravityTypes = GravityTypes.ObjectsLike;
+  _gravityAttraction: number = 0.01;
   _isGravity: boolean = true;
   constructor(object: PhysicObject) {
     this._object = object;
@@ -19,6 +19,14 @@ export default class GravitySystem {
   getState(): boolean {
     return this._isGravity;
   }
+  attract(other: PhysicObject) {
+    const p5 = this._object._p5;
+    let force = P5.Vector.sub(this._object._position, other._position);
+    let distancesq = p5.constrain(force.magSq(), 10, 100);
+    let power = this._gravityAttraction * (this._object._mass * other._mass);
+    force.setMag(power);
+    other.applyForce(force);
+  }
   applyGravity() {
     switch (this._gravityType) {
       case GravityTypes.EarthLike:
@@ -27,26 +35,13 @@ export default class GravitySystem {
         this._object.applyForce(gravity);
         break;
       case GravityTypes.ObjectsLike:
-        let objects = this._object._context.getOtherObjects(this._object._id);
+        var objects = this._object._context.getOtherObjects(this._object._id);
         for (let i = 0; i < objects.length; i++) {
-          //
-          if (
-            !TypeCheck.isRigidBody(objects[i]) ||
-            !TypeCheck.isPhysicObject(objects[i])
-          ) {
-            continue;
+          if (TypeCheck.isPhysicObject(objects[i])) {
+            let obj = objects[i] as PhysicObject;
+            this.attract(obj);
           }
-          let obj = objects[i] as PhysicObject;
-          //
-          let force = P5.Vector.sub(obj._position, this._object._position);
-          let distSq = this._object._p5.constrain(force.magSq(), 25, 2500);
-          let forcePower =
-            (this._gravityAttraction * (this._object._mass * obj._mass)) /
-            distSq;
-          force.setMag(forcePower);
-          this._object.applyForce(force);
         }
-        break;
       case GravityTypes.None:
         break;
     }
--- SimplePhyscisEngine/src/app.ts
+++ SimplePhyscisEngine/src/app.ts
@@ -2,28 +2,30 @@ import P5, { Vector } from "p5";
 import Walker from "./Modules/Objects/Dynamic/Walker";
 import Context from "./Modules/Context/Context";
 import Mouse from "./Modules/Objects/Dynamic/Mouse";
-import TargetObj from "./Modules/Objects/Dynamic/TargetObj";
 import { GravityTypes } from "./Modules/Enums/Forces/RigidBodyTypes";
 import GravityAttractor from "./Modules/Objects/StaticsObjects/GravityAttractor";
 const sketch = (p5: P5) => {
   const context = new Context();
-  const mouse = new Mouse(p5);
-  const attractor = new GravityAttractor(p5, 300, 300).setMass(2);
-  context.addObject(mouse);
-  context.addObject(attractor);
+
   p5.setup = () => {
     p5.createCanvas(800, 800);
-  };
-  p5.draw = () => {
-    p5.background(0);
-    p5.mouseClicked = () => {
-      const walker = new Walker(p5, p5.mouseX, p5.mouseY);
-      walker._gravitySystem._gravityType = GravityTypes.ObjectsLike;
-      walker.setVelocityLimit(5).setMass(p5.random(1, 10));
-      walker.applyForce(new Vector(p5.random(1, 10), p5.random(1, 10)));
+    for (let i = 0; i < 5; i++) {
+      const walker = new Walker(
+        p5,
+        p5.random(100, 200),
+        p5.random(100, 300)
+      ).setMass(p5.random(1, 10));
       walker._dragForceSystem.setEnabled(false);
       context.addObject(walker);
-    };
+    }
+
+    p5.background(0);
+  };
+  p5.draw = () => {
+    p5.background(0, 122);
+
+    p5.mouseClicked = () => {};
+    //
     if (p5.keyIsDown(p5.UP_ARROW)) {
       context.setWind(new Vector(1, 0));
     } else {
